b34ba309dbc5907ae37fd34f216b554e5e438a95
henriqueoliv999 <henrique.s.oliveira1998@gmail.com>
2022-03-29 18:36:40 -0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int fun1(int a, int b);
+int j=1;
+
+int main(){
+  int i, j;
+  int a = 5;
+  if(a % 2 == 0)
+    a = 2;
+  else
+    a = 3;
+  printf("%d\n", fun1(2,4));
+  for(i = 1; i<3; i++){
+    for(j= 1; j <3; j++){
+      printf("%d\n", fun1(a, i+j));
+    }
+  }
+}
+
+int fun1(int a, int b){
+  int i, p=1;
+  for(i=1; i<=b; i++){
+    p = p*a;
+  }
+return p+j;  
+}
